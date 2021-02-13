9bc4cd9472565aaae09c7550e4d0d6060d13cde1
Mish <mishuct26@gmail.com>
2021-02-13 11:45:50 +0200

    all necessary modules imports

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const sqlite = require('sqlite').verbose();
+const sqlite = require('sqlite').verbose()
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
 
 var db = new sqlite.Database('./database/employees.db')
