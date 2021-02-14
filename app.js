e34e225165bf0b8aaa7da95345dc1e962ec21406
Mish <mishuct26@gmail.com>
2021-02-14 22:41:18 +0200

    Set path to directory to server via express

--- app.js
+++ app.js
@@ -13,3 +13,4 @@ var db = new sqlite.Database('./database/employees.db');
 //app.use()- tells express to use piece of middleware
 //Middleware is a function Express passes req through before sending them to your routing func
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(_dirname,'./public')));
