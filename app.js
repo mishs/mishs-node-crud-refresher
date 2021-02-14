569045feff166709f8ecf159eaed0dba342eda34
Mish <mishuct26@gmail.com>
2021-02-14 22:35:26 +0200

    Returns middleware parsing urlenncoded bodies

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ const rateLimit = require('express-rate-limit')
 
 const app = express()
 const server = http.createServer(app)
-var db = new sqlite.Database('./database/employees.db')
+var db = new sqlite.Database('./database/employees.db');
+
+//app.use()- tells express to use piece of middleware
+//Middleware is a function Express passes req through before sending them to your routing func
+app.use(bodyParser.urlencoded({extended: false}));
